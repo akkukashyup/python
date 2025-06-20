1ce08bd6f16ff7fc2f24ac8cd2bd06299a0c88b7
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-20 06:56:32 +0530

    Add files via upload

---
+++ 3progarm.py
@@ -0,0 +1,17 @@
+#data tyoes
+name = "akanksha"
+age = 21
+price = 10.99
+a = None
+old = True
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(a))
+print(type(old))
+#name1 = 'ak'
+#name2 = "ak"
+#name3 = "'ak'"
+#print(name1)
+#print(name2)
+#print(name3)
