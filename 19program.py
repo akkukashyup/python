cd1cf4420c81a3e409528adbb4f6ffeb5a55a234
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-23 11:04:55 +0530

    Add files via upload

---
+++ 19program.py
@@ -0,0 +1,32 @@
+# list methods
+
+list = [3, 4, 6, 9, 5]
+
+list.append(7)
+print(list)
+
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
+
+list.insert(1,8)
+print(list)
+
+
+name = ["a", "d", "h", "e", "l"]
+
+name.sort()
+print(name)
+
+name.reverse()
+print(name)
+
+name.remove("d")
+print(name)
+
+name.pop(2)
+print(name)
+
+
