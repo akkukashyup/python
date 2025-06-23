026e2f4c89f209c47c9f7df0b3c26c3c32392933
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-23 11:04:03 +0530

    Add files via upload

---
+++ 20program.py
@@ -0,0 +1,28 @@
+# tuples 
+
+tup =(5, 6, 5, 0, 1,) # put last elment (,) is optional
+
+print(tup)
+print(type(tup))
+
+#indexing
+print(tup[4])
+print(tup[0])
+
+#empty tuple
+a = ()
+print(a)
+
+#single tuple
+b = (5,)
+print(b)
+
+# they are not tuple
+c = (5)
+print(c)
+print(type(c))
+
+# slicing
+
+print(tup[0:4])
+print(tup[-4:-2])
