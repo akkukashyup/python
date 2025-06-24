6736585ed8e088b30449629fee308061b312bb59
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-24 22:28:46 +0530

    Add files via upload

---
+++ 28program.py
@@ -0,0 +1,19 @@
+# set in python
+
+# set are mutable but set elements are immutable
+# each element has be unique
+
+collection = {1, 2, 2, "hello", "world", "world", 4}
+
+print(collection)
+
+print(len(collection))
+# total number of items 
+
+
+
+# empty set , sytax
+
+call = set()
+print(call)
+print(type(call))
