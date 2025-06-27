ec28a838bce2cbcdf5d9dd0260fe552c27364054
Akanksha Kashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-27 23:21:47 +0530

    Add files via upload

---
+++ 30program.py
@@ -0,0 +1,20 @@
+# union set
+
+set1 = { 1, 2, 3}
+set2 = { 2, 3, 4}
+
+print(set1.union(set2))
+# its reture {1,2,3,4}
+
+print(set1)
+print(set2)
+
+
+# intersection set
+
+
+print(set1.intersection(set2))
+# its reture {2,3}
+
+print(set1)
+print(set2)
