0e990ebcbef09e360e2ad79d732f024a3419024b
Akanksha Kashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-27 23:21:07 +0530

    Add files via upload

---
+++ 29program.py
@@ -0,0 +1,34 @@
+# set methods
+
+
+items = set()
+
+# add
+
+items.add(1)
+items.add(2)
+items.add(3)
+items.add("apple")
+items.add(("banana"))
+
+
+print(items)
+print(len(items))
+
+# remove
+
+items.remove(1)
+print(items)
+
+
+# clear
+
+"""items.clear()
+print(len(items))"""
+
+
+# pop
+
+print(items.pop())
+
+
